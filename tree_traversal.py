5ef2cac201c7e9ce3260f2258a37859a900cdf27
muturghutur87-cpu <muturghutur87@gmail.com>
2026-04-22 05:48:23 +0530

    Create tree_traversal.py

---
+++ tree_traversal.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+class Node:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def level_order(root):
+    """
+    Traverses a binary tree level by level using a Queue (BFS).
+    """
+    if not root: return []
+    res, queue = [], deque([root])
+
+    while queue:
+        level = []
+        for i in range(len(queue)):
+            node = queue.popleft()
+            level.append(node.val)
+            if node.left: queue.append(node.left)
+            if node.right: queue.append(node.right)
+        res.append(level)
+    return res
+
+# Complexity: Time O(n), Space O(n)
